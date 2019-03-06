d056f092862280b964660a247eaaf906d0489459
Juha Reunanen <juha.reunanen@tomaattinen.com>
2019-03-06 17:35:19 +0200

    Make AVX2 be the baseline; disable dispatching

--- vs/3rdparty/libjpeg-turbo/jconfigint.h
+++ vs/3rdparty/libjpeg-turbo/jconfigint.h
@@ -1,11 +1,19 @@
 /* libjpeg-turbo build number */
-#define BUILD  "20190216"
+#define BUILD  "opencv-4.0.1-dev-libjpeg-turbo"
 
 /* Compiler's inline keyword */
 #undef inline
 
 /* How to obtain function inlining. */
-#define INLINE  __forceinline
+#ifndef INLINE
+#if defined(__GNUC__)
+#define INLINE inline __attribute__((always_inline))
+#elif defined(_MSC_VER)
+#define INLINE __forceinline
+#else
+#define INLINE
+#endif
+#endif
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME  "libjpeg-turbo"
